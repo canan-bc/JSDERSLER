bbdcd07f5a56aad51fc4b3d98b0e6de7cd6a0039
Canan <useryedek56@gmail.com>
2025-11-30 21:49:13 +0300

    ilk commit

---
+++ 3. Çıktı Verme/app.js
@@ -0,0 +1,51 @@
+//Document write ile çıktı verme
+
+// document.write("Hello World <br>");
+// document.write("Canan");
+// document.write(null);
+// document.write(10);
+
+
+
+//console log metodu
+
+// console.log("Hello World");
+// console.log("merhaba dünya");
+// console.log(6.54);
+// console.log(true);
+// console.log({ name: "Bulut", surname: "sımarık" });
+
+// console.clear();
+
+
+// let a = 5;
+// let b = 10;
+// console.log("ikis sayının toplamı :", a + b);
+// console.clear();
+
+
+//Alert popup ile çıktı verme işlemleri
+
+// console.log(window)
+
+// let a = 10;
+// let b = 52;
+// alert("iki sayının toplamı:", (a + b));
+// alert("Merhaba");
+
+// alert("F5 Tuşuna Baymayınız")
+
+// console.log(window);
+
+// console.log(window.location.port);
+
+
+//Yorum Satiri
+
+
+/*
+rtgfre
+dfg
+dfg
+fdgfdg
+*/
